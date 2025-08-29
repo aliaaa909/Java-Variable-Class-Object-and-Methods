f49c6f0029f05cf5a867779064f880168ef09345
liaaaa ୨୧ <2410631170006@student.unsika.ac.id>
2025-08-29 12:33:21 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,15 @@
+public class Mahasiswa{
+    public void membaca(){
+        System.out.println("mahasiswa memabaca");
+    }
+
+    public void menyontek(){
+        System.out.println("mahasiswa menyontek");
+    }
+
+    public void modifikasi(){
+        System.out.println("mahasiswa modifiksi");
+    }
+    
+    
+}
---
+++ aksiMahasiswa.java
@@ -0,0 +1,11 @@
+public class aksiMahasiswa {
+    public static void main(String[] args) {
+        Mahasiswa mahasiswa = new Mahasiswa();
+
+        mahasiswa.membaca();
+        mahasiswa.menyontek();
+        mahasiswa.modifikasi();
+    
+        
+    }
+}
